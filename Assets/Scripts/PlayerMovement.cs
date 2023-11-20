e834e65cb7bcc3ef3a4582422716f6a5f8362f58
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2023-11-20 14:57:55 -0500

    Project built

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CustomInput input = null;
+    private Vector2 moveVector = Vector2.zero;
+    private Rigidbody2D rb;
+    private float moveSpeed = 10f;
+
+
+    private void Awake()
+    {
+        input = new CustomInput();
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Player.Movement.performed += OnMovementPeformed;
+        input.Player.Movement.canceled += OnMovementCancelled;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        input.Player.Movement.performed -= OnMovementPeformed;
+        input.Player.Movement.canceled -= OnMovementCancelled;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveVector * moveSpeed;
+    }
+
+    private void OnMovementPeformed(InputAction.CallbackContext value)
+    {
+        moveVector = value.ReadValue<Vector2>();
+    }
+
+    private void OnMovementCancelled(InputAction.CallbackContext value)
+    {
+        moveVector = Vector2.zero;
+    }
+}
